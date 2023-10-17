bd9f09972e462ed03f903e798a9b0e4b9e1928ad
akhmadhurshetov <122173327+akhmadhurshetov@users.noreply.github.com>
2023-10-17 17:53:11 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,12 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-#include <iostream>
-#include "task_1.h"
-
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    FamilyMember* member1 = new Child("Ahmad", 19, "NewUU");
+    FamilyMember* member2 = new Parent("Azamat", 45, "Accountant");
+
+    member1->display();
+    member2->display();
+
+    delete member1;
+    delete member2;
+
     return 0;
 }
